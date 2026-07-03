d24b44e06aa88adeeb2c4e5548ab6a1e4358ded4
Khyel <khyelcalanuga@gmail.com>
2026-07-03 16:20:58 +0800

    Remove sitemap plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { sitemap } from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: 'https://khyelcalanuga.dev',
-    }),
-  ],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
