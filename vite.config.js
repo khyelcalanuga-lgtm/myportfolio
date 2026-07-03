d59cb77b4bb8ad55f549b28fec875df2b8a3a4ed
Khyel <khyelcalanuga@gmail.com>
2026-07-03 16:07:02 +0800

    Add vite-plugin-sitemap for improved SEO; update package dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sitemap } from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://khyelcalanuga.dev',
+    }),
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
